d0a666163ffce623379efcab66f6151a8ce4b067
long <315202350@qq.com>
2019-06-18 21:12:44 +0800

    优化

--- liulangdiqiu.py
+++ liulangdiqiu.py
@@ -2,6 +2,7 @@
 import urllib.request
 import re
 import threading
+import requests
 import time
 import os
 
@@ -14,28 +15,26 @@ def download(url, name):
     print(name)
   except:
     print("error : " + url)
-
+imgurl = input("输入链接地址：")
 min_util = input("输入下限：")
 max_util = input("输入上限：")
+sleep_s = float(input("输入下载间隔（s）："))
 if_cover_exist_file = input("当发现文件存在是否覆盖(y/n)：")
 try:
   min_util = int(min_util)
   max_util = int(max_util)
-  contact_id = "001"
   for i in range(min_util, max_util + 1):
-    if i < 10:
-      contact_id = "00" + str(i)
-    elif i < 100:
-      contact_id = "0" + str(i)
-    else:
-      contact_id = str(i)
-    imgurl = "https://doubanzyv4.tyswmp.com:888/2019/02/07/06SfTJsUSa6nKqG9/out" + contact_id + ".ts"
+    # {:0>2d}     # 左侧补0
+    # >>> "{:0>4d}".format(1)
+    # '0001'
+    imgurl = imgurl.format(i)
+    # "https://doubanzyv4.tyswmp.com:888/2019/02/07/06SfTJsUSa6nKqG9/out" + contact_id + ".ts"
     # print(imgurl)
     try: 
       # urllib.request.urlretrieve(imgurl, '%s.ts' % (contact_id))
       if not os.path.exists(default_path):
         os.makedirs(default_path)
-      download_path = default_path + '%s.ts' % (contact_id)
+      download_path = default_path + '%s.ts' % (i)
       if os.path.exists(download_path) and (if_cover_exist_file == 'y'):
         os.remove(download_path)
       if not os.path.exists(download_path):
@@ -43,7 +42,7 @@ try:
           urllib.request.urlopen(imgurl)
           p = threading.Thread(target=download,args=(imgurl, download_path))
           p.start()
-          time.sleep(0.5)
+          time.sleep(sleep_s)
         except:
           "ok"
     except:  
